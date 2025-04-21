66de067c28190c6956d2a5a2d8e2982b82ccdd5f
星星不见我 <164742031+hbhkl19@users.noreply.github.com>
2025-04-21 21:05:43 +0800

    Add files via upload

---
+++ assignment4/Clock/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clock
+{
+
+    public class ClockTime
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+
+        public ClockTime()
+        {
+            Hour = 0;
+            Minute = 0;
+            Second = 0;
+        }
+        public ClockTime(int hour ,int minute , int second)
+        {
+            this.Hour = hour;
+            this.Minute = minute;
+            this.Second = second;
+        }
+
+        public bool isValid()
+        {
+            return Hour >= 0 && Minute >= 0 && Second >= 0
+                && Hour < 24 && Minute < 60 && Second < 60;
+        }
+
+        public  string Tostring()
+        {
+            return Hour + ":" + Minute + ":" + Second;
+        }
+    }
+
+
+    public class AlarmClock
+    {
+
+        public event Action<AlarmClock> AlarmEvent;
+
+        public event Action<AlarmClock> TickEvent;
+
+        private bool stop = false;
+
+        public AlarmClock()
+        {
+            CurrentTime = new ClockTime();
+            //为两个事件设置一个内部的处理函数
+            TickEvent += c => Console.WriteLine("Tick!");
+            AlarmEvent += c => Console.WriteLine("Ding! Ding! Ding!");
+        }
+
+        public ClockTime CurrentTime { get; set; }
+
+        public ClockTime AlarmTime { get; set; }
+
+        /// <summary>
+        /// 启动时钟，持续运行，直到stop变为false
+        /// </summary>
+        public void Run()
+        {
+            Console.WriteLine("The clock started!");
+            stop = false;
+            while (!stop)
+            {
+                DateTime now = DateTime.Now;
+                CurrentTime = new ClockTime(now.Hour, now.Minute, now.Second);
+                TickEvent(this);
+                if (AlarmTime.Equals(CurrentTime)) AlarmEvent(this);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("The clock stopped!");
+        }
+
+        public void Stop()
+        {
+            this.stop = true;
+        }
+    }
+
+
+    class MainClass
+    {
+
+        static void Main(string[] args)
+        {
+             AlarmClock clock = new AlarmClock(); //时钟
+             clock.AlarmTime = new ClockTime(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second + 5);
+             clock.AlarmEvent += PlayMusic;
+             int sum = 0;
+             clock.TickEvent += (c => sum = sum + 1);
+             new Thread(clock.Run).Start(); 
+             Console.WriteLine("press any key to stop.");
+             Console.ReadKey();
+             clock.Stop();
+             Console.WriteLine($"Sum= {sum}");
+
+        }
+
+        private static void Count(AlarmClock clock, ref int sum)
+        {
+            sum = sum + 1;
+        }
+
+        public static void PlayMusic(AlarmClock clock)
+        {
+            ClockTime time = clock.CurrentTime;
+            Console.WriteLine($"Alarm Event: {time.Hour}:{time.Minute}:{time.Second}");
+            Console.WriteLine("Playing music....");
+        }
+    }
+}
---
+++ assignment4/GenericList/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace GenericList
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            head = tail = null;
+        }
+
+        public void Add(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+            if (tail == null)
+            {
+                head = tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+        }
+
+        public void ForEach(Action<T> action)
+        {
+            Node<T> current = head;
+            for(; current != null; current = current.Next)
+            {
+                action(current.Value);
+            }
+        }
+
+        public double Sum(Func<T, double> selector)
+        {
+            double sum = 0;
+            Node<T> current = head;
+            for(; current != null; current = current.Next)
+            {
+                sum += selector(current.Value);
+            }
+            return sum;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //生成列表
+            Random random = new Random();
+            GenericList<int> list = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(random.Next(1, 100));
+            }
+
+            //打印列表内容
+            Console.WriteLine("List contents:");
+            list.ForEach(value => Console.WriteLine(value));
+
+            //求和
+            double sum = list.Sum(value => value);
+            Console.WriteLine($"Sum of list values: {sum}");
+
+            //求最小值和最大值
+            double min = 1000, max = -1;
+            list.ForEach(value =>
+            {
+                if (value < min) min = value;
+                if (value > max) max = value;
+            });
+            Console.WriteLine($"Min value: {min}");
+            Console.WriteLine($"Max value: {max}");
+
+        }
+    }
+}
